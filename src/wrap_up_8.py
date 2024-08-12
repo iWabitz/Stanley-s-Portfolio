7c2b02c44cb757b677a76d7cc3b317af31f48b52
iWabitz <shiqizhu321@gmail.com>
2024-08-12 16:54:09 -0700

    Update wrap_up_8.py

--- src/wrap_up_8.py
+++ src/wrap_up_8.py
@@ -1,5 +1,8 @@
 import streamlit as st
 def wrap_up():
+    st.balloons()
+    VIDEO_URL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    st.video(VIDEO_URL)
     st.header(":package:. Wrap up")
     st.write("Great job! You've learned the basics of streamlit.")
     st.write("Remember you can make amazing web apps with just a few lines of Python code.")
